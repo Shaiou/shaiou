415d463b603be52a46145bbd66fed0c5c3b15d39
Shailendra Narayen <snarayen@vente-privee.com>
2018-07-14 17:24:39 +0200

    pimps

--- packages/install.sh
+++ packages/install.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt install -y python-software-properties software-properties-common
+apt install -y python-software-properties software-properties-common
 for repo in $(cat repositories.txt)
 do
     apt-add-repository $repo   
 done
-sudo apt update -qq
-sudo apt -y upgrade
+apt update -qq
+apt -y upgrade
 
-sudo apt install -y $(cat apt.txt)
+for package in $(cat apt.txt)
+do
+    apt install -y $package
+done
+
 pip install -r pip.txt
 for gorepo in $(cat go.txt)
 do
