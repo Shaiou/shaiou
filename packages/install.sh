fe0c43a17bd41ab0b5806451146fa536df67bf3e
Shailendra NARAYEN <shailendra.narayen@manomano.com>
2019-09-17 10:47:10 +0200

    Clean up packages for Manjaro

--- packages/install.sh
+++ packages/install.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
-apt install -y python-software-properties software-properties-common
-for repo in $(cat repositories.txt)
+for package in $(cat pacman.txt)
 do
-    apt-add-repository $repo   
-done
-apt update -qq
-apt -y upgrade
-
-for package in $(cat apt.txt)
-do
-    apt install -y $package
+    pacman -Sy $package
 done
 
 pip install -r pip.txt
+
 for gorepo in $(cat go.txt)
 do
     go get -u $gopkg
