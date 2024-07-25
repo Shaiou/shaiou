15ba0a3f6be85cdbc273f916747588977c941838
Shailendra Narayen <shailendra.narayen@believe.com>
2024-07-25 15:07:49 +0200

    feat(battery): update script to check battery and add image and doc for crontab

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-BAT=$(acpi -b | grep "Battery 0" | egrep -E -o '[0-9][0-9]?%')
+BAT=$(acpi -b | grep "Battery 0" | grep -E -o '[0-9][0-9]?%')
 
 # Full and short texts
 echo "Battery: $BAT"
 echo "BAT: $BAT"
 
 # Set urgent flag below 5% or use orange below 20%
-[ ${BAT%?} -le 5 ] && exit 33
 [ ${BAT%?} -le 15 ] && notify-send -i ~/Icons/low-battery.png "${BAT%?} % remaining"
-[ ${BAT%?} -le 20 ] && echo "#FF8000"
 
 exit 0
