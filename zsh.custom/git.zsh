a8e141fa7fe11a006244a6c946660601ebd55870
Shailendra Narayen <shailendra.narayen@believe.com>
2024-12-27 16:01:29 +0100

    (git.sh): fix mr creation for gitlab

--- zsh.custom/git.zsh
+++ zsh.custom/git.zsh
@@ -3,4 +3,4 @@ alias groot='cd $(git rev-parse --show-toplevel)'
 alias gc="git checkout"
 alias gforce="git commit --amend --no-edit; git push -f"
 alias gd="git diff --word-diff"
-alias merge="lab mr create -d"
+alias merge="glab mr create"
