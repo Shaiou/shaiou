46ab4701ac4e03a060c84f9d359d9c16fff94deb
Shailendra Narayen <shailendra.narayen@something.com>
2025-03-13 08:11:46 +0100

    Revert "chore(): update packages + bat + plugin load"
    This reverts commit 6ce8cabd999b8968e7dbc73e07f366a991f7736d.

--- zsh.custom/shell.zsh
+++ zsh.custom/shell.zsh
@@ -9,12 +9,11 @@ export GOPATH=/Workspace/Go
 export PATH=$PATH:$GOPATH/bin
 
 # Aliases
-alias cat=bat
+alias myip="/usr/bin/curl ifconfig.co 2>/dev/null"
+alias ll='ls -larth'
+alias vi='vim'
 alias diff='difft'
 alias grep='grep --color=always'
-alias ll='ls -larth'
-alias myip="/usr/bin/curl ifconfig.co 2>/dev/null"
-alias vi='vim'
 ## Key layout
 alias fr="setxkbmap fr"
 alias us="setxkbmap us"
@@ -29,32 +28,16 @@ do
 done
 
 # Mise autocomplete
-eval "$(/usr/bin/mise activate zsh)"
+eval "$(/home/shailendra/bin/mise activate zsh)"
 
 # Prompt
 export ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Plugins
-plugins=(
-    argocd
-    aws
-    azure
-    direnv
-    docker
-    docker-compose
-    git
-    jira
-    kubectl
-    mise
-    ssh-agent
-    terraform
-)
+plugins=(argocd aws azure direnv docker docker-compose jira kubectl mise terraform)
 # Autocomplete stuff
 fpath=(~/zsh.d $fpath)
 zstyle ':completion:*' completer _complete _ignored _approximate
 zstyle :compinstall filename '/home/shaiou/.zshrc'
-# Load ssh agent
-zstyle :omz:plugins:ssh-agent lazy yes
-
 autoload -Uz compinit
 compinit
--- zsh.custom/ssh.zsh
+++ zsh.custom/ssh.zsh
@@ -1,3 +1,6 @@
+# Load ssh agent
+zstyle :omz:plugins:ssh-agent lazy yes
+
 function sshadd {
     ssh-add /Workspace/Keys/$1
 }
